adb240d40685ac1051a1678b8a1e035d3f599c1b
Даниил Карнаухов <starshiy06@vk.com>
2025-06-01 01:11:40 +0300

    01.06 - 1ый этап, рабочая ссылка

--- js/JavaScript.js
+++ js/JavaScript.js
@@ -1,3 +1,3 @@
 ﻿let d = new Date();
-alert('*** WebSite ***');
-document.body.innerHTML = "<h1 style='color:azure'>Сегодня " + d + "</h1>"
+alert('***  Ку-Ку ***');
+document.body.innerHTML = "Ку, Красотка"
